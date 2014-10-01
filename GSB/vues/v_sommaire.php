ccdf15ae5de155580b589d7b523a2538af2f60c6
JeremyBonnard <jeremybonnard42@gmail.com>
2014-10-01 21:31:16 +0200

    Changement dans v_sommaire pour qu'il vérifie le nom ET le prénom, ajout de c_modifierImmat mais quasi rien dedans

--- GSB/vues/v_sommaire.php
+++ GSB/vues/v_sommaire.php
@@ -1,32 +1,61 @@
 ﻿    <!-- Division pour le sommaire -->
+
     <div id="menuGauche">
+
      <div id="infosUtil">
+
     
+
         <h2>
+
     
+
 </h2>
+
     
+
       </div>  
+
         <ul id="menuList">
+
 			<li >
+
 				  Visiteur :<br>
+
 				<?php echo $_SESSION['prenom']."  ".$_SESSION['nom']  ?>
+
 			</li>
+
            <li class="smenu">
+
               <a href="index.php?uc=gererFrais&action=saisirFrais" title="Saisie fiche de frais ">Saisie fiche de frais</a>
+
            </li>
+
            <li class="smenu">
+
               <a href="index.php?uc=etatFrais&action=selectionnerMois" title="Consultation de mes fiches de frais">Mes fiches de frais</a>
+
            </li>
+
  	   <li class="smenu">
+
               <a href="index.php?uc=connexion&action=deconnexion" title="Se déconnecter">Déconnexion</a>
-           </li>
-           <?php  if ( $_SESSION['prenom']="daf" ){ ?>          
+
+           </li>           
+           <?php  if ( $_SESSION['prenom']=="David" and $_SESSION['nom']=="Andre" ){ ?>            
      <li class="smenu">
+
              <a href="index.php?uc=connexion&action=modifierimmat" title="Changer une immatriculation"> Changer une immatriculation</a>
+
            </li> 
+
            <?php } ?>
+
          </ul>
+
         
+
     </div>
+
     
