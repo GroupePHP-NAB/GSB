093ccd04cb04671e4b314e8bb9d82a08e44817cb
BonnardJeremy <jeremylesacri@hotmail.fr>
2014-10-03 12:03:28 +0200

    Ajout de la case 'valideChangementId'

--- GSB/controleurs/c_modifierImmat.php
+++ GSB/controleurs/c_modifierImmat.php
@@ -1,4 +1,14 @@
 <?php
 include("vues/v_sommaire.php");
 include("vues/v_modifier.php");
+$action = $_REQUEST['action'];
+switch($action){
+		case 'valideChangementId':{
+			$nom= $_POST['Nom'];
+			$prenom=$_POST['Prenom'];
+			$immat=$_POST['Immatriculation']
+			VerifExistanceVisiteur($nom,$prenom);
+			majImmat($idVisiteur,$immat);
+		}
+}
 ?>
