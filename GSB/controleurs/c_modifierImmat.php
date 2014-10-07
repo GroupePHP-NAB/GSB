5fd7615fe03cf92800fa3548c33e603144c77b28
BonnardJeremy <jeremylesacri@hotmail.fr>
2014-10-07 14:57:52 +0200

    Des modif

--- GSB/controleurs/c_modifierImmat.php
+++ GSB/controleurs/c_modifierImmat.php
@@ -3,12 +3,21 @@ include("vues/v_sommaire.php");
 include("vues/v_modifier.php");
 $action = $_REQUEST['action'];
 switch($action){
-		case 'valideChangementId':{
+		case 'validerChangementImmat':{
 			$nom= $_POST['Nom'];
+			echo $nom;
 			$prenom=$_POST['Prenom'];
-			$immat=$_POST['Immatriculation']
-			VerifExistanceVisiteur($nom,$prenom);
-			majImmat($idVisiteur,$immat);
+			echo $prenom;
+			$immat=$_POST['Immatriculation'];
+			echo $immat;
+			$idVisiteur=$pdo->verifExistanceVisiteur($nom,$prenom);
+			echo $idVisiteur;
+			if(isset($idVisiteur)){
+				include("vues/v_erreurs.php");
+			}
+			else{
+				$pdo->majImmat($idVisiteur,$immat);
+			}
 		}
 }
 ?>
